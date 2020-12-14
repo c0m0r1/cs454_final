a9daa51c33d0f3742e5d6e5a7823b51eef3c4bbb
KimDongok <ehddhr4454@kaist.ac.kr>
2020-12-13 16:45:06 -0800

    modify domato tester

--- domato_test.py
+++ domato_test.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from coverage import fitness
+from coverage import fitness, get_coverage_info
 from GP.html_seeder import generate_html_tree
 
 with open("domato_sample.html", "r") as f:
     data = f.read()
 print(fitness(data))
+print(get_coverage_info(data))
