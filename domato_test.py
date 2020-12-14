7d726680c617e61976273c2126b7ba345063dcc0
KimDongok <ehddhr4454@kaist.ac.kr>
2020-12-14 04:08:41 -0800

    add more domato test cases

--- domato_test.py
+++ domato_test.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
 from coverage import fitness, get_coverage_info
-from GP.html_seeder import generate_html_tree
+import os
 
-with open("domato_sample.html", "r") as f:
-    data = f.read()
-print(fitness(data))
-print(get_coverage_info(data))
+def domato_test(dir):
+    max_f = float('-inf')
+    result = None
+    for fname in os.listdir(dir):
+        with open(os.path.join(dir,fname), "r") as f:
+            data = f.read()
+
+        new_f = fitness(data)
+
+        if new_f > max_f:
+            max_f = new_f
+            result = data 
+    return (max_f, result)
+        
+if __name__ == "__main__":
+    result = domato_test("./domato_samples")
+    print(result)
+    print(get_coverage_info(result[1]))
